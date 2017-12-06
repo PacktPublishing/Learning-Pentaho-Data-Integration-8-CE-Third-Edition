2e08afd93a528caa27b6e565cb8c3776ad9b9433
sayli2212 <saylin@packtpub.com>
2017-12-06 11:25:30 +0530

    Code files added
    Code files added

---
+++ Chapter 11/scripts/postgreSQL/js_dw.sql
@@ -0,0 +1,152 @@
+﻿-- CREATE DATABASE js_dw;
+
+CREATE TABLE LK_THEMES (
+                id integer NOT NULL,
+                theme CHAR(50) NOT NULL,
+                PRIMARY KEY (id)
+);
+
+-- ALTER TABLE LK_THEMES COMMENT '';
+
+
+CREATE TABLE LK_PIECES (
+                id integer NOT NULL,
+                min integer DEFAULT 0 NOT NULL,
+                max integer DEFAULT 0 NOT NULL,
+                description CHAR(30) NOT NULL
+);
+
+-- ALTER TABLE LK_PIECES COMMENT '';
+
+
+CREATE TABLE LK_REGIONS_2 (
+                id integer NOT NULL,
+                city CHAR(30) DEFAULT 'N/A' NOT NULL,
+                country CHAR(30) DEFAULT 'N/A' NOT NULL,
+                region CHAR(30) DEFAULT 'N/A' NOT NULL,
+                id_js integer DEFAULT 0 NOT NULL,
+                start_date DATE,
+                end_date DATE,
+                version integer DEFAULT 1 NOT NULL,
+                current CHAR(1) DEFAULT 'Y' NOT NULL,
+                lastupdate DATE,
+                PRIMARY KEY (id)
+);
+
+-- ALTER TABLE LK_REGIONS_2 COMMENT '';
+
+
+CREATE TABLE FT_PUZZ_SALES (
+                date integer NOT NULL,
+                id_manufacturer integer NOT NULL,
+                id_region integer NOT NULL,
+                id_junk_prod integer NOT NULL,
+                id_puzzle integer NOT NULL,
+                id_pieces integer NOT NULL,
+                quantity integer NOT NULL
+);
+
+-- ALTER TABLE FT_PUZZ_SALES COMMENT '';
+
+
+CREATE TABLE FT_SALES (
+                date integer NOT NULL,
+                id_manufacturer integer NOT NULL,
+                id_region integer NOT NULL,
+                id_junk_sales integer NOT NULL,
+                product_type CHAR(10) NOT NULL,
+                quantity integer DEFAULT 0 NOT NULL,
+                amount NUMERIC(8,2) DEFAULT 0 NOT NULL
+);
+
+-- ALTER TABLE FT_SALES COMMENT '';
+-- COMMENT ON COLUMN FT_SALES.product_type IS 'puzzle or accesory';
+
+
+CREATE TABLE LK_JUNK_SALES (
+                id integer NOT NULL,
+                buy_method CHAR(25) NOT NULL,
+                payment_method CHAR(25) NOT NULL,
+                PRIMARY KEY (id, buy_method, payment_method)
+);
+
+-- ALTER TABLE LK_JUNK_SALES COMMENT '';
+
+
+CREATE TABLE LK_JUNK_PROD (
+                id integer NOT NULL,
+                glowsInDark CHAR(1) NOT NULL,
+                is3D CHAR(1) NOT NULL,
+                wooden CHAR(1) NOT NULL,
+                isPanoramic CHAR(1) NOT NULL,
+                nrPuzzles smallint NOT NULL,
+                PRIMARY KEY (id, glowsInDark, is3D, wooden, isPanoramic, nrPuzzles)
+);
+
+-- ALTER TABLE LK_JUNK_PROD COMMENT '';
+
+
+CREATE TABLE LK_PUZZLES (
+                id integer NOT NULL,
+                name CHAR(35) DEFAULT 'N/A' NOT NULL,
+                theme CHAR(50) DEFAULT 'N/A' NOT NULL,
+                id_js_prod CHAR(8) DEFAULT 00000000 NOT NULL,
+                id_js_man CHAR(3) DEFAULT 000 NOT NULL,
+                start_date DATE,
+                end_date DATE,
+                version integer DEFAULT 1 NOT NULL,
+                current CHAR(1) DEFAULT 'Y' NOT NULL,
+                lastupdate DATE,
+                PRIMARY KEY (id)
+);
+
+-- ALTER TABLE LK_PUZZLES COMMENT '';
+
+
+CREATE TABLE LK_MANUFACTURERS (
+                id integer NOT NULL,
+                name CHAR(25) DEFAULT 'N/A' NOT NULL,
+                id_js CHAR(3) NOT NULL,
+                lastupdate DATE,
+                PRIMARY KEY (id)
+);
+
+-- ALTER TABLE LK_MANUFACTURERS COMMENT '';
+
+
+CREATE TABLE LK_REGIONS (
+                id integer NOT NULL,
+                city CHAR(30) DEFAULT 'N/A' NOT NULL,
+                country CHAR(30) DEFAULT 'N/A' NOT NULL,
+                region CHAR(30) DEFAULT 'N/A' NOT NULL,
+                id_js integer NOT NULL,
+                lastupdate DATE,
+                PRIMARY KEY (id)
+);
+
+-- ALTER TABLE LK_REGIONS COMMENT '';
+
+CREATE TABLE LK_TIME_SIMPLE (
+                dateid integer NOT NULL,
+                year integer NOT NULL,
+                month smallint NOT NULL,
+                day smallint NOT NULL,
+                week_day smallint NOT NULL,
+                PRIMARY KEY (dateid)
+);
+
+
+CREATE TABLE LK_TIME (
+                dateid integer NOT NULL,
+                year integer NOT NULL,
+                month smallint NOT NULL,
+                day smallint NOT NULL,
+                week_day smallint NOT NULL,
+                week_desc CHAR(10) NOT NULL,
+                week_short_desc CHAR(3) NOT NULL,
+                month_desc CHAR(10) NOT NULL,
+                month_short_desc CHAR(3) NOT NULL,
+                PRIMARY KEY (dateid)
+);
+
+-- ALTER TABLE LK_TIME COMMENT 'Time Dimension';
---
+++ README.md
@@ -0,0 +1,40 @@
+# Learning Pentaho Data Integration 8 CE
+This is the code repository for [Learning Pentaho Data Integration 8 CE](https://www.packtpub.com/big-data-and-business-intelligence/learning-pentaho-data-integration-8-ce?utm_source=github&utm_medium=repository&utm_campaign=9781788292436), published by [Packt](https://www.packtpub.com/?utm_source=github). It contains all the supporting project files necessary to work through the book from start to finish.
+## About the Book
+Pentaho Data Integration(PDI) is an intuitive and graphical environment packed with drag-and-drop design and powerful Extract-Tranform-Load (ETL) capabilities.
+
+This book shows and explains the new interactive features of Spoon, the revamped look and feel, and the newest features of the tool including Transformations and Jobs Executors and the invaluable Metadata Injection capability.
+
+We begin with the installation of PDI software and then move on to cover all the key PDI concepts. Each of the chapter introduces new features, enabling you to gradually get practicing with the tool. First, you will learn to do all kinds of data manipulation and work with simple plain files. Then, the book teaches you how you can work with relational databases inside PDI. Moreover, you will be given a primer on data warehouse concepts and you will learn how to load data in a data warehouse. During the course of this book, you will be familiarized with its intuitive, graphical and drag-and-drop design environment.
+
+By the end of this book, you will learn everything you need to know in order to meet your data manipulation requirements. Besides, your will be given best practices and advises for designing and deploying your projects.
+
+## Instructions and Navigation
+All of the code is organized into folders. Each folder starts with a number followed by the application name. For example, Chapter02.
+
+
+
+The code will look like the following:
+```
+project_name,start_date,end_date
+Project A,2016-01-10,2016-01-25
+Project B,2016-04-03,2016-07-21
+Project C,2017-01-15,???
+Project D,2015-09-03,2015-12-20
+Project E,2016-05-11,2016-05-31
+Project F,2011-12-01,2013-11-30
+```
+
+PDI is a multiplatform tool. This means that no matter which operating system you have, you will be able to work with the tool. The only prerequisite is to have JVM 1.8 installed. You will also need an Office suite, for example, Open Office or Libre Office, and a good text editor, for example, Sublime III or Notepad ++. Access to a relational database is recommended. Suggested engines are MySQL and PostgreSQL, but could be others of your choice as well.
+
+Having an internet connection while reading is extremely useful too. Several links are provided throughout the book that complements what is explained. Besides, there is the PDI forum where you may search or post doubts if you are stuck with something.
+
+## Related Products
+* [Dockerization - Do more with Docker [Integrated Course]](https://www.packtpub.com/virtualization-and-cloud/dockerization-do-more-docker-integrated-course?utm_source=github&utm_medium=repository&utm_campaign=9781788394857)
+
+* [Mastering Machine Learning Algorithms](https://www.packtpub.com/big-data-and-business-intelligence/mastering-machine-learning-algorithms?utm_source=github&utm_medium=repository&utm_campaign=9781788621113)
+
+* [Learning JavaScript Data Structures and Algorithms - Third Edition](https://www.packtpub.com/web-development/learning-javascript-data-structures-and-algorithms-third-edition?utm_source=github&utm_medium=repository&utm_campaign=9781788623872)
+
+### Suggestions and Feedback
+[Click here](https://docs.google.com/forms/d/e/1FAIpQLSe5qwunkGf6PUvzPirPDtuy1Du5Rlzew23UBp2S-P3wB-GcwQ/viewform) if you have any feedback or suggestions.
